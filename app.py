33533c6bf0e23d8f672131106f5cd684d1539ce7
Kristijan Bakaric <kbakaric1@gmail.com>
2020-03-10 22:18:40 +0100

    Add sample code

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
 
-# db.create_all()
 
 # ----------------------------------------------------------------------------#
 # Models.
@@ -38,15 +37,22 @@ class Venue(db.Model):
     __tablename__ = "venue"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.Column(db.ARRAY(db.String(120)))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String)
+    num_past_shows = db.Column(db.Integer)
+    num_upcoming_shows = db.Column(db.Integer)
+    # area_id = db.Column(db.Integer, db.ForeignKey("area.id"), nullable=True)
+    shows = db.relationship("Show", backref="venue")
 
 
 class Artist(db.Model):
@@ -57,14 +63,28 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.Column(db.ARRAY(db.String))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String)
+    upcoming_shows_count = db.Column(db.Integer)
+    past_shows_count = db.Column(db.Integer)
+    shows = db.relationship("Show", backref="artist")
 
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+
+class Show(db.Model):
+    __tablename__ = "show"
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey("venue.id"), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey("artist.id"), nullable=False)
+    start_time = db.Column(db.String, nullable=False)
+
 
 # ----------------------------------------------------------------------------#
 # Filters.
